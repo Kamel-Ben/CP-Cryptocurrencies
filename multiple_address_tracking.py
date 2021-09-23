bac8f82fade319a39fc9b79adbda5a161de6028e
Kamel <kamel.benbrik59@gmail.com>
2021-09-23 23:58:44 +0200

    Create multiple_address_tracking.py

---
+++ multiple_address_tracking.py
@@ -0,0 +1,43 @@
+import discord, config, time, requests
+from discord.embeds import Embed
+from discord.ext import tasks
+
+
+client = discord.Client()
+adresses = ['address']
+names = ["whale"]
+value = [[] for _ in range(len(adresses))]
+
+@tasks.loop(seconds = 1)
+async def test():
+    channel = client.get_channel('num_channel') #put channel number here 
+    while True:
+        for position, (adress, name) in enumerate(zip(adresses,names)): 
+            url = f"https://api.bscscan.com/api?module=account&action=txlist&address={adress}&startblock=0&endblock=99999999&page=1&offset=1&sort=desc&apikey={config.API_KEY_BSC}"
+            data = requests.get(url.format(adress)).json().get("result")
+
+            time.sleep(1)
+            if len(data)>0:
+                last_transaction_value = int(data[0].get("value"))/(10**18)
+                value[position].append(float(last_transaction_value))
+            else:
+                continue
+
+            if len(value[position])>1:
+                if value[position][-1] != value[position][-2] :
+                    last_transaction_from = data[0].get("from")
+                    last_transaction_to = data[0].get("to")
+                    await channel.send(f"**Alerte - Nouvelle Transaction**\n\n\
+**Name**: {name}\n\
+**From**: {last_transaction_from}\n\
+**BNB amount**: {last_transaction_value}\n\
+**To**: {last_transaction_to}\n\
+**Link**: <https://bscscan.com/address/{adress}>")
+
+@client.event
+async def on_ready():
+    print("Le bot est prêt")
+    test.start()
+
+client.run('') #méthode run - Token du BOT 
+
